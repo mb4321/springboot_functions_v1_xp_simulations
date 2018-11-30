b9bcc3238cf7153633434155291840af681f49cb
Martin Brown <mbrown5151@googlemail.com>
2018-11-30 11:50:55 +0000

    Initial SpringBoot project via Spring Initializr

---
+++ src/main/java/com/martin/springboot_functions_v1_xp_simulations/SpringbootFunctionsV1XpSimulationsApplication.java
@@ -0,0 +1,12 @@
+package com.martin.springboot_functions_v1_xp_simulations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFunctionsV1XpSimulationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFunctionsV1XpSimulationsApplication.class, args);
+	}
+}
